e87916fec95339802ca37f6bbf8eebdadc4396ae
Maruf Hansat <57494393+marufhasnat@users.noreply.github.com>
2024-12-18 22:56:25 +0600

    Add files via upload

---
+++ E-Commerce.DataAccess/Repository/IRepository/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using E_commerce_Web_API.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DataAccess.Repository.IRepository
+{
+    public interface ICategoryService
+    {
+        List<CategoryReadDto> GetAllCategories();
+        CategoryReadDto? GetCategoryById(Guid categoryId);
+        CategoryReadDto CreateCategory(CategoryCreateDto categoryData);
+        CategoryReadDto? UpdateCategoryById(Guid categoryId, CategoryUpdateDto categoryData);
+        bool DeleteCategoryById(Guid categoryId);
+    }
+}
